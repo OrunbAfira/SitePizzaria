ce69c1e0892d926075b22f6a59ad3f62407b5dfa
OrunbAfira <71826773+OrunbAfira@users.noreply.github.com>
2024-03-19 15:55:45 -0300

    Add files via upload

---
+++ cardapio.php
@@ -0,0 +1,104 @@
+<?php require 'header.php' ?>
+<link rel="stylesheet" href="index.css">
+<body>
+<div class="container">
+    <h1 id="card1" class="text-center my-5">Cardápio de Pizzaria</h1>
+    <!-- Pizza Doce -->
+    <div class="card my-3">
+      <div class="card-body">
+        <h5 class="card-title">Pizzas Doces</h5>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Sabor</th>
+              <th>Tamanho</th>
+              <th>Preço</th>
+            </tr>
+          </thead>
+          <body>
+            <tr>
+              <td>Chocolate com Morango</td>
+              <td>Pequena</td>
+              <td>R$ 25,00</td>
+            </tr>
+            <tr>
+              <td>Banoffee</td>
+              <td>Média</td>
+              <td>R$ 30,00</td>
+            </tr>
+            <tr>
+              <td>M&M</td>
+              <td>Grande</td>
+              <td>R$ 35,00</td>
+            </tr>
+          </body>
+        </table>
+      </div>
+    </div>
+  
+    <!-- Pizza Salgada -->
+    <div class="card my-3">
+      <div class="card-body">
+        <h5 class="card-title">Pizzas Salgadas</h5>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Sabor</th>
+              <th>Tamanho</th>
+              <th>Preço</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Calabresa</td>
+              <td>Pequena</td>
+              <td>R$ 20,00</td>
+            </tr>
+            <tr>
+              <td>Margherita</td>
+              <td>Média</td>
+              <td>R$ 25,00</td>
+            </tr>
+            <tr>
+              <td>Portuguesa</td>
+              <td>Grande</td>
+              <td>R$ 30,00</td>
+            </tr>
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  
+    <!-- Bebidas -->
+    <div class="card my-3">
+      <div class="card-body">
+        <h5 class="card-title">Bebidas</h5>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Preço</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Refrigerante (lata)</td>
+              <td>R$ 5,00</td>
+            </tr>
+            <tr>
+              <td>Água Mineral (500ml)</td>
+              <td>R$ 3,00</td>
+            </tr>
+            <tr>
+              <td>Suco Natural (copo)</td>
+              <td>R$ 7,00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+   
+
+  </body>
---
+++ faleConosco.php
@@ -0,0 +1,12 @@
+<?php require 'header.php' ?>
+<link rel="stylesheet" href="index.css">
+<form>
+  <div class="col-md-3 offset-md-0">
+    <label for="exampleFormControlInput1">Seu Email</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com">
+  </div>
+  <div class="col-md-3 offset-md-0">
+    <label for="exampleFormControlTextarea1">Fale Conosco</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
